eb6f3d1a5a3ec04d9c9da20e6941f77e77264987
Tareq-dev <tareque.dev@gmail.com>
2025-07-18 22:03:19 +0600

    product banner sizing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className="navbar bg-[#642771] text-white shadow-sm md:px-8">
+        <div className="navbar bg-[#642771] h-21 text-white shadow-sm md:px-8">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- frontend/src/components/ProductsBannerTwo.jsx
+++ frontend/src/components/ProductsBannerTwo.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import banner1 from "../assets/gdg_banner1.jpg";
 import banner2 from "../assets/gdg_banner2.jpg";
- 
+
 
 
 function ProductsBannerTwo() {
   return (
     <div className='flex justify-center my-8'>
-        <div className="md:flex gap-3">
-      <img className="w-[400px] h-[300px] object-cover rounded-lg" src={banner1} alt="" />
-      <img className="w-[400px] h-[300px]  object-cover rounded-lg" src={banner2} alt="" />
-      <img className="w-[400px] h-[300px]  object-cover rounded-lg" src={banner2} alt="" />
-    </div>
+      <div className="md:flex gap-8">
+        <img className="w-[600px] h-[400px] object-cover rounded-lg" src={banner1} alt="" />
+        <div className='flex flex-col gap-1.5'>
+          <img className="w-[600px] h-[200px]  object-cover rounded-lg" src={banner2} alt="" />
+          <img className="w-[600px] h-[200px]  object-cover rounded-lg" src={banner2} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
